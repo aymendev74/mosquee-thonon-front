1ca9d4738c9119f2657349d1aeec75d6011bfb2f
aymendev74 <84038990+aymendev74@users.noreply.github.com>
2024-05-02 21:52:53 +0200

    Update AxiosConfig.ts

--- src/services/AxiosConfig.ts
+++ src/services/AxiosConfig.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 const AxiosInstance = axios.create();
 
 AxiosInstance.interceptors.request.use((config) => {
-    // config.baseURL = "https://www.inscription-mosquee-thonon.org/api/v1/";
-    config.baseURL = "http://localhost:8080/api/v1/";
+    config.baseURL = "https://www.inscription-mosquee-thonon.org/api/v1/";
+    // config.baseURL = "http://localhost:8080/api/v1/";
     const token = sessionStorage.getItem("token");
     if (token) {
         config.headers.Authorization = "Bearer " + token;
