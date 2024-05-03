0ecfaaca5861660f2d6be482cd1f71270408e558
Aymen <aymen330d@hotmail.fr>
2024-05-03 19:30:04 +0200

    externalisation baseurl api

--- src/services/AxiosConfig.ts
+++ src/services/AxiosConfig.ts
@@ -3,8 +3,7 @@ import axios from "axios";
 const AxiosInstance = axios.create();
 
 AxiosInstance.interceptors.request.use((config) => {
-    config.baseURL = "https://www.inscription-mosquee-thonon.org/api/v1/";
-    // config.baseURL = "http://localhost:8080/api/v1/";
+    config.baseURL = process.env.REACT_APP_BASE_URL_API;
     const token = sessionStorage.getItem("token");
     if (token) {
         config.headers.Authorization = "Bearer " + token;
