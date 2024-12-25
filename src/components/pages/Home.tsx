45807c17973babc3d87e5498bc8446d1d531ca23
Aymen <aymen330d@hotmail.fr>
2024-12-25 19:10:45 +0100

    css

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -11,21 +11,25 @@ export const Home: FunctionComponent = () => {
                         Vous pouvez vous inscrire ou inscrire vos enfants aux cours dispensés par l'association en remplissant le formulaire.<br />
                         Vous pouvez également devenir en adhérent en remplissant le formulaire prévu à cet effet.
                     </h3>
+                    <div className="logo-carousel hidden-xs" />
                 </div>
                 <div>
                     <h3 className="carousel-element">
                         Si vous souhaitez inscrire vos enfants aux cours de langue arabe dispensés par l'association,<br />
                         il vous suffit de remplir le formulaire en accédant au menu dédié ou en cliquant <a href="/coursEnfants">ici</a>.
                     </h3>
+                    <div className="logo-carousel" />
                 </div>
                 <div>
                     <h3 className="carousel-element">
                         Si vous souhaitez vous inscrire aux cours de langue arabe pour adultes dispensés par l'association,<br />
                         il vous suffit de remplir le formulaire en accédant au menu dédié, ou en cliquant <a href="/coursAdultes">ici</a>.
                     </h3>
+                    <div className="logo-carousel" />
                 </div>
                 <div>
                     <h3 className="carousel-element">Vous pouvez également soutenir l'association en devenant adhérent de l'AMC, en vous inscrivant via le menu dédié ou en cliquant <a href="/adhesion">ici</a>.<br /></h3>
+                    <div className="logo-carousel" />
                 </div>
             </Carousel>
         </div>
